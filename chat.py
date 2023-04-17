5885084085b244369240ffe0dce9715ed403453e
avill <avill.vg@gmail.com>
2023-04-17 13:32:37 -0600

    first commit

---
+++ chat.py
@@ -0,0 +1,24 @@
+import openai #pip install openai
+import os
+
+import customtkinter as ctk
+
+
+def openaiChat(prompt):
+    # Set OpenAI API key
+	openai.api_key = os.getenv("OPENAI_API_KEY")
+	# Define prompt and parameters
+	model = "text-davinci-002"
+	max_tokens = 50
+
+	# Generate text using GPT-3 API
+	response = openai.Completion.create(
+		engine=model,
+		prompt=prompt,
+		max_tokens=max_tokens,
+	)
+	# Print generated text
+	print(response.choices[0].text.strip())
+
+prompt = "What is your name?"
+openaiChat(prompt=prompt)
